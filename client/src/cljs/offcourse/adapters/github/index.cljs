cf0fb61d10a076d6e01cf174e9a6a4cbd205edf6
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-09-02 00:39:08 +0200

    * client/build.boot:

---
+++ client/src/cljs/offcourse/adapters/github/index.cljs
@@ -0,0 +1,16 @@
+(ns offcourse.adapters.github.index
+  (:require [com.stuartsierra.component :refer [Lifecycle]]
+            [offcourse.adapters.github.send :as send-impl]
+            [shared.protocols.responsive :as ri :refer [Responsive]]
+            [shared.protocols.queryable :refer [Queryable]]
+            [services.logger :as logger]))
+
+(defrecord Github []
+  Lifecycle
+  (start [db] db)
+  (stop  [db] db)
+  Responsive
+  (-send [db event] (send-impl/send db event)))
+
+(defn new-db [config]
+  (map->Github config))
